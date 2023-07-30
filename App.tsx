d8e39188d9ff400847dd9c01a431af74f89ba742
John Sierra <sierrajp73@gmail.com>
2023-07-30 15:41:05 -0400

    initial commit

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default App;
