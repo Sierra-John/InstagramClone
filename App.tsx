4a27422ceb550ecd65fffc79166ef99617340fd3
John Sierra <sierrajp73@gmail.com>
2023-07-30 16:10:49 -0400

    Added basic fonts and colors

--- App.tsx
+++ App.tsx
@@ -1,9 +1,13 @@
 import {View, Text} from 'react-native';
+import colors from './src/theme/colors';
+import font from './src/theme/fonts';
 
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text>Hello World!</Text>
+      <Text style={{color: colors.primary, fontSize: font.size.lg}}>
+        Hello World!
+      </Text>
     </View>
   );
 };
