f91d31634fdad645c5c6487672d96da38b4ec8f2
John Sierra <sierrajp73@gmail.com>
2023-07-30 16:30:44 -0400

    Set up react-native-vector-icons

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import {View, Text} from 'react-native';
 import colors from './src/theme/colors';
 import font from './src/theme/fonts';
+import Antdesign from 'react-native-vector-icons/AntDesign';
 
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text style={{color: colors.primary, fontSize: font.size.lg}}>
         Hello World!
+        <Antdesign name="stepforward" size={25} />
       </Text>
     </View>
   );
