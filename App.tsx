d82a78a6cb38983b102f60dd768f65a621d6d4e0
John Sierra <sierrajp73@gmail.com>
2023-07-31 11:11:01 -0400

    Created Component for Post

--- App.tsx
+++ App.tsx
@@ -1,150 +1,20 @@
-import {View, Text, StyleSheet, Image} from 'react-native';
-import colors from './src/theme/colors';
-import fonts from './src/theme/fonts';
+import {View, StyleSheet, ScrollView} from 'react-native';
 
-import Entypo from 'react-native-vector-icons/Entypo';
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import Feather from 'react-native-vector-icons/Feather';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import FeedPost from './src/components/FeedPost';
 
 const App = () => {
   return (
-    <View style={styles.post}>
-      {/* Header */}
-      <View style={styles.header}>
-        <Image
-          source={{
-            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
-          }}
-          style={styles.userAvatar}
-        />
-        <Text style={styles.userName}>vadimnotjustdev</Text>
-        <Entypo
-          name="dots-three-horizontal"
-          size={16}
-          style={styles.threeDots}
-        />
-      </View>
-
-      {/* Content */}
-      <Image
-        source={{
-          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-        }}
-        style={styles.image}
-      />
-
-      {/* Footer */}
-      <View style={styles.footer}>
-        {/* Icons */}
-        <View style={styles.iconContainer}>
-          <AntDesign
-            name={'hearto'}
-            size={24}
-            style={styles.icon}
-            color={colors.black}
-          />
-          <Ionicons
-            name="chatbubble-outline"
-            size={24}
-            style={styles.icon}
-            color={colors.black}
-          />
-          <Feather
-            name="send"
-            size={24}
-            style={styles.icon}
-            color={colors.black}
-          />
-          <Feather
-            name="bookmark"
-            size={24}
-            style={{marginLeft: 'auto'}}
-            color={colors.black}
-          />
-        </View>
-
-        {/* Likes */}
-        <Text style={styles.text}>
-          Liked by <Text style={styles.bold}>lgrinevicius</Text> and{' '}
-          <Text style={styles.bold}>66 others</Text>
-        </Text>
-
-        {/* Post Description */}
-        <Text style={styles.text}>
-          <Text style={styles.bold}>vadimnotjustdev</Text> Lorem ipsum dolor,
-          sit amet consectetur adipisicing elit. Totam sequi quae, fugit
-          quibusdam inventore quaerat est tempora similique laudantium non!
-          Deserunt voluptatem suscipit iure esse est aliquam aut quisquam
-          inventore.
-        </Text>
-
-        {/* Comments */}
-        <Text style={{color: 'grey'}}>View all 16 comments</Text>
-        <View style={styles.comment}>
-          <Text style={styles.commentText}>
-            <Text style={styles.bold}>vadimnotjustdev</Text> Lorem ipsum dolor,
-            sit amet consectetur adipisicing elit.
-          </Text>
-          <AntDesign name={'hearto'} style={styles.icon} color={colors.black} />
-        </View>
-
-        {/* Posted Date */}
-        <Text>19 December, 2021</Text>
-      </View>
-    </View>
+    <ScrollView style={styles.app}>
+      <FeedPost />
+      <FeedPost />
+      <FeedPost />
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
-  post: {},
-  image: {
-    width: '100%',
-    aspectRatio: 1,
-  },
-  header: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
-  },
-  userAvatar: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    marginRight: 10,
-  },
-  userName: {
-    fontWeight: fonts.weight.bold,
-    color: colors.black,
-  },
-  threeDots: {
-    marginLeft: 'auto',
-  },
-  footer: {
-    padding: 10,
-  },
-  iconContainer: {
-    flexDirection: 'row',
-    marginBottom: 5,
-  },
-  icon: {
-    marginHorizontal: 5,
-  },
-  text: {
-    color: colors.black,
-    lineHeight: 18,
-  },
-  bold: {
-    fontWeight: fonts.weight.bold,
-  },
-  commentText: {
-    color: colors.black,
+  app: {
     flex: 1,
-    lineHeight: 18,
-  },
-  comment: {
-    flexDirection: 'row',
-    alignItems: 'center',
   },
 });
 
---
+++ src/components/FeedPost/FeedPost.tsx
@@ -0,0 +1,100 @@
+import {View, Text, StyleSheet, Image} from 'react-native';
+import colors from '../../theme/colors';
+import fonts from '../../theme/fonts';
+import styles from './styles';
+
+import Entypo from 'react-native-vector-icons/Entypo';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import Feather from 'react-native-vector-icons/Feather';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+const FeedPost = () => {
+  return (
+    <View style={styles.post}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Image
+          source={{
+            uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/1.jpg',
+          }}
+          style={styles.userAvatar}
+        />
+        <Text style={styles.userName}>vadimnotjustdev</Text>
+        <Entypo
+          name="dots-three-horizontal"
+          size={16}
+          style={styles.threeDots}
+        />
+      </View>
+
+      {/* Content */}
+      <Image
+        source={{
+          uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
+        }}
+        style={styles.image}
+      />
+
+      {/* Footer */}
+      <View style={styles.footer}>
+        {/* Icons */}
+        <View style={styles.iconContainer}>
+          <AntDesign
+            name={'hearto'}
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Ionicons
+            name="chatbubble-outline"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="send"
+            size={24}
+            style={styles.icon}
+            color={colors.black}
+          />
+          <Feather
+            name="bookmark"
+            size={24}
+            style={{marginLeft: 'auto'}}
+            color={colors.black}
+          />
+        </View>
+
+        {/* Likes */}
+        <Text style={styles.text}>
+          Liked by <Text style={styles.bold}>lgrinevicius</Text> and{' '}
+          <Text style={styles.bold}>66 others</Text>
+        </Text>
+
+        {/* Post Description */}
+        <Text style={styles.text}>
+          <Text style={styles.bold}>vadimnotjustdev</Text> Lorem ipsum dolor,
+          sit amet consectetur adipisicing elit. Totam sequi quae, fugit
+          quibusdam inventore quaerat est tempora similique laudantium non!
+          Deserunt voluptatem suscipit iure esse est aliquam aut quisquam
+          inventore.
+        </Text>
+
+        {/* Comments */}
+        <Text style={{color: 'grey'}}>View all 16 comments</Text>
+        <View style={styles.comment}>
+          <Text style={styles.commentText}>
+            <Text style={styles.bold}>vadimnotjustdev</Text> Lorem ipsum dolor,
+            sit amet consectetur adipisicing elit.
+          </Text>
+          <AntDesign name={'hearto'} style={styles.icon} color={colors.black} />
+        </View>
+
+        {/* Posted Date */}
+        <Text>19 December, 2021</Text>
+      </View>
+    </View>
+  );
+};
+
+export default FeedPost;
