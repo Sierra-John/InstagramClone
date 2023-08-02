6fd284d1b62faa15fd3c0d9216f75e442d532f13
John Sierra <sierrajp73@gmail.com>
2023-08-02 12:33:40 -0400

    Expanded Comment Component to render in HomeScreen and CommentsScreen

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import {View, StyleSheet, Text} from 'react-native';
-import HomeScreen from './src/screens/HomeScreen/HomeScreen';
+import HomeScreen from './src/screens/HomeScreen';
+import CommentsScreen from './src/screens/CommentsScreen';
 
 const App = () => {
   return (
     <View style={styles.app}>
-      <HomeScreen />
+      <CommentsScreen />
     </View>
   );
 };
--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,5 +1,5 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {View, Text, StyleSheet, Image, Pressable} from 'react-native';
+import {useState} from 'react';
 import fonts from '../../theme/fonts';
 import colors from '../../theme/colors';
 import AntDesign from 'react-native-vector-icons/AntDesign';
@@ -7,16 +7,42 @@ import {IComment} from '../../types/models';
 
 interface ICommentProps {
   comment: IComment;
+  includeDetails: boolean;
 }
 
-const Comment = ({comment}: ICommentProps) => {
+const Comment = ({comment, includeDetails = false}: ICommentProps) => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLike = () => {
+    setIsLiked(currentValue => !currentValue);
+  };
+
   return (
     <View style={styles.comment}>
-      <Text style={styles.commentText}>
-        <Text style={styles.bold}>{comment.user.username}</Text>
-        {' ' + comment.comment}
-      </Text>
-      <AntDesign name={'hearto'} style={styles.icon} color={colors.black} />
+      {includeDetails && (
+        <Image source={{uri: comment.user.image}} style={styles.avatar} />
+      )}
+
+      <View style={styles.middleColumn}>
+        <Text style={styles.commentText}>
+          <Text style={styles.bold}>{comment.user.username}</Text>
+          {' ' + comment.comment}
+        </Text>
+        {includeDetails && (
+          <View style={styles.footer}>
+            <Text style={styles.footerText}>2d</Text>
+            <Text style={styles.footerText}>5 likes</Text>
+            <Text style={styles.footerText}>Reply</Text>
+          </View>
+        )}
+      </View>
+      <Pressable onPress={toggleLike} hitSlop={5}>
+        <AntDesign
+          name={isLiked ? 'heart' : 'hearto'}
+          style={styles.icon}
+          color={isLiked ? colors.accent : colors.black}
+        />
+      </Pressable>
     </View>
   );
 };
@@ -28,14 +54,29 @@ const styles = StyleSheet.create({
   bold: {
     fontWeight: fonts.weight.bold,
   },
-  commentText: {
-    color: colors.black,
-    flex: 1,
-    lineHeight: 18,
-  },
   comment: {
     flexDirection: 'row',
     alignItems: 'center',
+  },
+  commentText: {
+    color: colors.black,
+    lineHeight: 18,
+  },
+  avatar: {
+    width: 40,
+    aspectRatio: 1,
+    borderRadius: 25,
+    marginRight: 5,
+  },
+  middleColumn: {
+    flex: 1,
+  },
+  footer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  footerText: {
+    marginRight: 10,
   },
 });
 
