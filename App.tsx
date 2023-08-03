c643fcdb0eff7310d61dab8aea82e14b1ac2babf
John Sierra <sierrajp73@gmail.com>
2023-08-03 17:37:44 -0400

    Created EditProfileScreen UI

--- App.tsx
+++ App.tsx
@@ -2,11 +2,12 @@ import {View, StyleSheet, Text} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import CommentsScreen from './src/screens/CommentsScreen';
 import ProfileScreen from './src/screens/ProfileScreen';
+import EditProfileScreen from './src/screens/EditProfileScreen';
 
 const App = () => {
   return (
     <View style={styles.app}>
-      <ProfileScreen />
+      <EditProfileScreen />
     </View>
   );
 };
