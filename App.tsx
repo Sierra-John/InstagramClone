b346f79b0774de4f017600c1a681bdf94304e50c
John Sierra <sierrajp73@gmail.com>
2023-08-03 13:30:05 -0400

    Created ProfilePage and various compoenets

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import {View, StyleSheet, Text} from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 import CommentsScreen from './src/screens/CommentsScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 
 const App = () => {
   return (
     <View style={styles.app}>
-      <CommentsScreen />
+      <ProfileScreen />
     </View>
   );
 };
